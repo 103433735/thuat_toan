4ccc8d000dbc9cc50c00749708da9b01596799ec
DucNguyen <ducnafs1579@gmail.com>
2024-08-19 14:12:50 +0700

    Bài 3: Phép so sánh pointer tổng hợp

--- ThuatToan/main.cpp
+++ ThuatToan/main.cpp
@@ -11,19 +11,28 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     int x = 10;
     int y = 20;
+    int z = 30;
+    
+    int* q = &z;
     int* p = &x;
+    p = q;
+    p = NULL;
     
-    //Gán lại pointer bằng một địa chỉ khác
-    p = &y;
+    //Phép so sánh pointer
+    cout << (p == &x) << endl; //p sẽ lưu địa chỉ NULL, mà &x lại chính là địa chỉ của x --> false
+    cout << (p != q) << endl; //p lưu địa chỉ NULL, q lưu địa chỉ của z --> true
+    cout << (p <= q) << endl;
+    cout << (p >= q) << endl;
+    cout << (p == NULL) << endl;
     
-    cout << *p << endl ;
-    int* q = p;
-    cout << *q << endl ;
-    
-    //Biến con trỏ 2 chiều
-    int** qq = &q;
-    cout << **qq << endl;
-    
+
+    //Check NULL cho một pointer
+    if (p != NULL) {
+        //Lúc này cho phép truy cập biến p
+        cout << *p << endl;
+    } else {
+        cout << "P bị NULL ròi má" << endl;
+    }
 }
 
 
