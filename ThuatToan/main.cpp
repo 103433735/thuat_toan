a714da76a5dc527275238b34be0d019edc751659
DucNguyen <ducnafs1579@gmail.com>
2024-08-19 17:30:45 +0700

    Bài 11: Template class và Template struct

--- ThuatToan/main.cpp
+++ ThuatToan/main.cpp
@@ -10,13 +10,33 @@ using namespace std;
 
 //Template
 template <typename T>
-void hello() {
-    cout << "Type: " << typeid(T).name() << endl;
-    cout << "Size: " << sizeof(T) << " bytes" << endl;
-}
+struct Point_struct {
+    T x;
+    T y;
+    
+    void showPoint() {
+        cout << "Type: " << typeid(T).name() << endl;
+    }
+};
+
+template <typename T>
+class Point_class {
+public:
+    T x;
+    T y;
+};
 
 int main(int argc, const char * argv[]) {
-    hello<int>();
+    Point_struct<int> point_struct;
+    point_struct.x = 10;
+    point_struct.y = 20;
+    
+    Point_class<float> point_class;
+    point_class.x = 30;
+    point_class.y = 40;
+    
+    // Gọi hàm bên trong struct với kiểu dữ liệu T
+    point_struct.showPoint();
 }
 
 
