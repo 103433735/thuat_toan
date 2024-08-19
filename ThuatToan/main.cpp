148909071ab692e89d94bcf75f5c6689f74b900c
DucNguyen <ducnafs1579@gmail.com>
2024-08-19 15:35:18 +0700

    Bài 5: Class và Struct trong C++

--- ThuatToan/main.cpp
+++ ThuatToan/main.cpp
@@ -8,14 +8,37 @@
 #include <iostream>
 using namespace std;
 
+struct Person_struct {
+    //Trong struct có thể khai báo kiểu function
+    char fullName[30];
+    int age = 30;
+    float height = 1.65;
+    
+    void hello() {
+        cout << "hello" << endl;
+    }
+};
+
+class Person_class {
+public: //access scope
+    char fullName[30];
+    int age;
+    float height;
+    void getFullName() {
+        cout << "Hi there " << fullName << endl;
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    //REFERENCE: Cơ chế này khác pointer ở chỗ, biến reference không lưu địa chỉ của một biến khác. Thay vào đó, nó tạo alias cho cái biến đó
-    int x = 10;
-    int* p = &x; //Pointer
+    Person_struct person_struct = {"Duc Nguyen",24, 1.99};
+    cout << person_struct.age << endl;
+    person_struct.hello();
     
-    int& r = x; //Reference
     
-    cout << r << endl;
+    Person_class person_class = {"Testing function", 24, 1.54};
+    cout << person_class.fullName << endl;
+    person_class.getFullName();
+    
 }
 
 
